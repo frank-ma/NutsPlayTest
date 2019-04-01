31839661fc0d33f48ec99c3161f8d41376bcf374
FRANK MA <frankma9103@gmail.com>
2019-04-01 14:59:16 +0800

    test,first commit

---
+++ nutplaysdk/src/main/java/com/nutplaysdk/sdk/TenUtils.java
@@ -0,0 +1,17 @@
+package com.nutplaysdk.sdk;
+
+/**
+ * Created by frank-ma on 2019/4/1 下午2:33
+ * Email: frankma9103@gmail.com
+ * Desc:
+ */
+public class TenUtils {
+
+    public static String Ten= "hello world";
+
+    public static void test2Gradle(){
+
+        System.out.println(Ten);
+
+    }
+}
